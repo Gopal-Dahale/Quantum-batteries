a2b29b91e37ae4c03d24de240639fedbc3eb4db9
Gopal Dahale <dahalegopal27@gmail.com>
2024-02-22 18:23:26 +0530

    Added documentation

--- rotoselect.py
+++ rotoselect.py
@@ -1,3 +1,7 @@
+"""
+CS-VQE with Rotoselect as optimizer
+"""
+
 import cudaq
 import pickle
 from time import time
@@ -12,6 +16,18 @@ cudaq.set_target("nvidia")
 
 ## Helper functions for Rotoselect optimizer
 def RGen(kernel, param, generator, wire):
+    """
+    Applies the given rotation gate `generator` on the qubit `wire`
+
+    Args:
+      kernel: The `cudaq.kernel`.
+      param: The parameter value of the rotation gate.
+      generator: Rotation gate name.
+      wire: The qubit on which the rotation gate is applied
+
+    Returns:
+      Updated `kernel`.
+    """
     if generator == "X":
         kernel.rx(param, wire)
     elif generator == "Y":
@@ -21,12 +37,35 @@ def RGen(kernel, param, generator, wire):
 
 
 def create_kernel(n_qubits):
+    """
+    Creats a cudaq kernel and allocates qubits
+
+    Args:
+      n_qubits: Number of qubits in the circuit.
+
+    Returns:
+        qubits: The allocated qubits based on `n_qubits`.
+        kernel: The `cudaq.kernel`.
+        parameters: Parameters required by the quantum circuit.
+    """
     kernel, parameters = cudaq.make_kernel(list)
     qubits = kernel.qalloc(n_qubits)
     return qubits, kernel, parameters
 
 
 def ansatz(generators, n_qubits, hf):
+    """
+    The Hardware efficient ansatz with a single rotation layer
+    followed by a CNOT ladder
+
+    Args:
+      generators: Set of Pauli rotation gates.
+      n_qubits: Number of qubits in the circuit.
+      hf: Hartree fock state.
+
+    Returns:
+        The `cudaq.kernel` with gates applied.
+    """
     qubits, kernel, parameters = create_kernel(n_qubits)
 
     # Prepare the Hartree Fock State.
@@ -43,8 +82,20 @@ def ansatz(generators, n_qubits, hf):
 
 
 def cost_fn(params, generators, n_qubits, hamiltonian, hf):
-    """Returns the expectation value as our cost."""
+    """
+    Calculates the energy expectation value of a given Hamiltonian using a specified
+    ansatz and parameters.
 
+    Args:
+      params: Parameters required by the quantum circuit.
+      generators: Set of Pauli rotation gates.
+      hamiltonian: System Hamiltonian.
+      hf:  Hartree fock state.
+
+    Returns:
+      The energy expectation value calculated based on the provided parameters,
+    generators, number of qubits, Hamiltonian, and Hartree-Fock state.
+    """
     kernel = ansatz(generators, n_qubits, hf)
     expectation_value = cudaq.observe(kernel, hamiltonian, params).expectation()
     return expectation_value
@@ -53,6 +104,22 @@ def cost_fn(params, generators, n_qubits, hamiltonian, hf):
 def rotosolve(
     d, params, generators, cost, M_0, n_qubits, hamiltonian, hf
 ):  # M_0 only calculated once
+    """
+    The rotosolve optimizer
+
+    Args:
+      d: the index of a parameter in the `params`.
+      params: Parameters required by the quantum circuit.
+      generators: Set of Pauli rotation gates.
+      cost: The energy expectation value function.
+      M_0: Energy expectation value whern `params[d]` is zero.
+      n_qubits: Number of qubits in the circuit.
+      hamiltonian: System Hamiltonian.
+      hf: Hartree fock state.
+
+    Returns:
+      The energy expectation value with updated parameters.
+    """
     params[d] = np.pi / 2.0
     M_0_plus = cost(params, generators, n_qubits, hamiltonian, hf)
     params[d] = -np.pi / 2.0
@@ -69,6 +136,23 @@ def rotosolve(
 def optimal_theta_and_gen_helper(
     d, params, generators, cost, n_qubits, hamiltonian, hf
 ):
+    """
+    The function iterates over different generators to find the optimal
+    parameters and generators for a given cost function.
+
+    Args:
+      d: the index of a parameter in the `params`.
+      params: Parameters required by the quantum circuit.
+      generators: Set of Pauli rotation gates.
+      cost: The energy expectation value function.
+      n_qubits: Number of qubits in the circuit.
+      hamiltonian: System Hamiltonian.
+      hf: Hartree fock state.
+
+    Returns:
+      The optimal value of the parameter `params[d]` (`params_opt_d`) and
+      the corresponding optimal generator choice (`generators_opt_d`).
+    """
     params[d] = 0.0
     M_0 = cost(
         params, generators, n_qubits, hamiltonian, hf
@@ -87,6 +171,20 @@ def optimal_theta_and_gen_helper(
 
 
 def rotoselect_cycle(cost, params, generators, n_qubits, hamiltonian, hf):
+    """
+    One cycle of Rotoselect algorithm
+
+    Args:
+      cost: The energy expectation value function.
+      params: Parameters required by the quantum circuit.
+      generators: Set of Pauli rotation gates.
+      n_qubits: Number of qubits in the circuit.
+      hamiltonian: System Hamiltonian.
+      hf: Hartree fock state.
+
+    Returns:
+      The updated `params` and `generators`.
+    """
     for d in range(len(params)):
         params[d], generators[d] = optimal_theta_and_gen_helper(
             d, params, generators, cost, n_qubits, hamiltonian, hf
@@ -95,7 +193,9 @@ def rotoselect_cycle(cost, params, generators, n_qubits, hamiltonian, hf):
 
 
 if __name__ == "__main__":
-    ccsd_energy = -3688.046308050882
+    ccsd_energy = -3688.046308050882  # Reference classical energy
+
+    # Load the contextuals hamiltonians and hartree fock states
     with open("CS_hams.pickle", "rb") as handle:
         hams = pickle.load(handle)
 
@@ -120,7 +220,7 @@ if __name__ == "__main__":
         ham.get_qubit_count() for ham, hf in zip(hamiltonians[::-1], hf_states[::-1])
     ]
 
-    num_iterations = 10
+    num_iterations = 10  # Number of experiments
 
     for ham, hf in zip(hamiltonians[::-1], hf_states[::-1]):
         n_qubits = ham.get_qubit_count()
@@ -141,6 +241,7 @@ if __name__ == "__main__":
             n_steps = 50
             parameter_count = n_qubits
 
+            # Optimzer loop
             start = time()
             for i in range(n_steps):
                 params_rsel, generators = rotoselect_cycle(
@@ -149,9 +250,12 @@ if __name__ == "__main__":
             energy = cost_fn(params_rsel, generators, n_qubits, ham, hf)
             end = time()
 
+            # store the relative error and runtime
             temp_rel_errs.append(rel_err(ccsd_energy, energy))
             temp_durations.append(end - start)
 
+        # Calculate the mean and standarad deviations for the
+        # relative error and runtime
         mean_durations.append(np.mean(temp_durations))
         mean_rel_errs.append(np.mean(temp_rel_errs))
 
@@ -164,6 +268,7 @@ if __name__ == "__main__":
         print(f"duration = {mean_durations[-1]} += {std_durations[-1]}")
         print(f"generators = {generators}")
 
+    # Plot the relative error and runtime
     fig, ax = plt.subplots(1, 2, figsize=(12, 5))
 
     ax[0].errorbar(num_qubits, mean_rel_errs, std_rel_errs, marker="o", capsize=4)
